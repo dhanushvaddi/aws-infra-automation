b3513b719aa4cde0f6a5f01dd5cfedf0494b4c71
dhanushvaddi <dhanushh.vaddi@gmail.com>
2025-04-20 00:59:05 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,14 +2,43 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "ec2" {
-  count         = 3
-  ami           = "ami-0c02fb55956c7d316"
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_instance" "ansible_master" {
+  ami           = "ami-0c55b159cbfafe1f0" # Ubuntu 22.04 LTS AMI
   instance_type = "t2.micro"
-  key_name      = ""
-  user_data     = file("${path.module}/ansible-install.sh")
+  subnet_id     = data.aws_subnet.default.id
+  key_name      = "your-key" # Change to your AWS key
 
   tags = {
-    Name = "Terraform-EC2-${count.index + 1}"
+    Name = "Ansible-Master"
+  }
+}
+
+resource "aws_instance" "jenkins_slave_1" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  subnet_id     = data.aws_subnet.default.id
+  key_name      = "your-key"
+
+  tags = {
+    Name = "Jenkins-Slave-1"
+  }
+}
+
+resource "aws_instance" "jenkins_slave_2" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  subnet_id     = data.aws_subnet.default.id
+  key_name      = "your-key"
+
+  tags = {
+    Name = "Jenkins-Slave-2"
   }
 }
